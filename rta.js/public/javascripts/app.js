79881162da44a556109b5d8a1dc1505b9e8a9f5d
dinesh <dinesh@crypsis.net>
2012-05-01 13:23:07 +0530

    added ts.py: containing general timeseries functions

--- rta.js/public/javascripts/app.js
+++ rta.js/public/javascripts/app.js
@@ -1036,71 +1036,6 @@ function');
   }
 }));
 (this.require.define({
-  "views/templates/symbols": function(exports, require, module) {
-    module.exports = function (__obj) {
-  if (!__obj) __obj = {};
-  var __out = [], __capture = function(callback) {
-    var out = __out, result;
-    __out = [];
-    callback.call(this);
-    result = __out.join('');
-    __out = out;
-    return __safe(result);
-  }, __sanitize = function(value) {
-    if (value && value.ecoSafe) {
-      return value;
-    } else if (typeof value !== 'undefined' && value != null) {
-      return __escape(value);
-    } else {
-      return '';
-    }
-  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
-  __safe = __obj.safe = function(value) {
-    if (value && value.ecoSafe) {
-      return value;
-    } else {
-      if (!(typeof value !== 'undefined' && value != null)) value = '';
-      var result = new String(value);
-      result.ecoSafe = true;
-      return result;
-    }
-  };
-  if (!__escape) {
-    __escape = __obj.escape = function(value) {
-      return ('' + value)
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;');
-    };
-  }
-  (function() {
-    (function() {
-      var item, _i, _len, _ref;
-    
-      __out.push('\n<select>\n  ');
-    
-      _ref = this.items;
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        item = _ref[_i];
-        __out.push('\n    <option value="');
-        __out.push(__sanitize(item.get('id')));
-        __out.push('"> ');
-        __out.push(__sanitize(item.get('name')));
-        __out.push(' </option>\n  ');
-      }
-    
-      __out.push('\n</select>\n  ');
-    
-    }).call(this);
-    
-  }).call(__obj);
-  __obj.safe = __objSafe, __obj.escape = __escape;
-  return __out.join('');
-}
-  }
-}));
-(this.require.define({
   "views/templates/sidebar": function(exports, require, module) {
     module.exports = function (__obj) {
   if (!__obj) __obj = {};
@@ -1195,6 +1130,71 @@ function');
         __out.push(__sanitize(item.get('id')));
         __out.push('" class=\'add btn btn-info\' value=\'Add\' />\n    \n  </li>\n');
       }
+    
+    }).call(this);
+    
+  }).call(__obj);
+  __obj.safe = __objSafe, __obj.escape = __escape;
+  return __out.join('');
+}
+  }
+}));
+(this.require.define({
+  "views/templates/symbols": function(exports, require, module) {
+    module.exports = function (__obj) {
+  if (!__obj) __obj = {};
+  var __out = [], __capture = function(callback) {
+    var out = __out, result;
+    __out = [];
+    callback.call(this);
+    result = __out.join('');
+    __out = out;
+    return __safe(result);
+  }, __sanitize = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else if (typeof value !== 'undefined' && value != null) {
+      return __escape(value);
+    } else {
+      return '';
+    }
+  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+  __safe = __obj.safe = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else {
+      if (!(typeof value !== 'undefined' && value != null)) value = '';
+      var result = new String(value);
+      result.ecoSafe = true;
+      return result;
+    }
+  };
+  if (!__escape) {
+    __escape = __obj.escape = function(value) {
+      return ('' + value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+    };
+  }
+  (function() {
+    (function() {
+      var item, _i, _len, _ref;
+    
+      __out.push('\n<select>\n  ');
+    
+      _ref = this.items;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        item = _ref[_i];
+        __out.push('\n    <option value="');
+        __out.push(__sanitize(item.get('id')));
+        __out.push('"> ');
+        __out.push(__sanitize(item.get('name')));
+        __out.push(' </option>\n  ');
+      }
+    
+      __out.push('\n</select>\n  ');
     
     }).call(this);
     
---
+++ rta/ts.py
@@ -0,0 +1,18 @@
+
+
+import numpy as np
+
+__all__ = ['roll_intersect', 'roll_trendline']
+
+def roll_intersect(x,y):
+  X, Y = np.asarray(x), np.asarray(y)
+  diff = X > Y
+  shifted_diff = np.insert( diff[:-1], 0, [False] )
+  indices = np.nonzero( np.logical_xor( diff, shifted_diff ) )[0]
+  return [ (i, x[i] > x[i+1] ) for i in indices if i < x.shape[0] -1 ]
+
+def roll_trendline(x, y, *args):
+  _x = np.arange( len(x) )
+  p = np.poly1d( np.polyfit(_x, y, 1) )
+  return [ [x[i], v] for i,v in enumerate(p(_x)) ]
+  
